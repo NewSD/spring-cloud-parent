18d84c99516d7834acfb142d2a05e007196eaf42
niuhaideng <niuhaideng@163.com>
2019-03-05 22:20:07 +0800

    hello service

---
+++ eureka-server/src/test/java/com/niu/AppTest.java
@@ -0,0 +1,12 @@
+package com.niu;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by ami on 2019/3/5.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+
+public class AppTest {
+}
---
+++ hello-service/src/test/java/com/niu/spring/AppTest.java
@@ -0,0 +1,20 @@
+package com.niu.spring;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HelloApplication.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
